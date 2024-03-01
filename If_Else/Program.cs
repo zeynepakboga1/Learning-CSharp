307578f4702d91ce3e485b5c0937b739ec05eaaf
Zeynep Akboğa <zeynepakboga2236@gmail.com>
2024-03-01 22:15:41 +0300

    Project initialization

---
+++ If_Else/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//number
+            var number = 10;
+            if (number == 10) ;
+            Console.WriteLine("number is 10");
+            //colour
+            string colour = "red";
+            if (colour == "blue")
+                Console.WriteLine("colour is blue");
+            else
+                Console.WriteLine("colour is red");
+
+            //single line if ile çalışmak
+
+             
+            Console.ReadLine();
+        }
+    }
+}
---
+++ veri tipleri/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            char character = 'd';
+            Console.WriteLine(character);
+            Console.WriteLine("character is {0}",(int) character);
+            bool condition = false;
+            int number1 = 80;
+            long number2 = 30000000000;
+            short number3 = 1200;
+            byte number4 = 0;
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(number4);
+            Console.WriteLine("numer4 is {0}", number4);
+            Console.WriteLine(number1);
+            Console.WriteLine("number1 is {0}",number1);
+            Console.WriteLine(number2);
+            Console.WriteLine("number2 is{0}",number2);
+            Console.WriteLine(number3);
+            Console.WriteLine("number3 is{0}", number3);
+           
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
