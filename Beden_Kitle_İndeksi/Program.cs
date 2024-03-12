9ba135ed838c116bd16a37548902f67c4bc12dba
Zeynep Akboğa <zeynepakboga2236@gmail.com>
2024-03-13 02:37:10 +0300

    Beden_Kitle_İndeksi

---
+++ Beden_Kitle_İndeksi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beden_Kitle_İndeksi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*beden kitle indeksi= kilo/boy*boy
+         beden kitle indeksi 18'e eşit veya küçükse zayıf
+        18 ile 25 arası normal
+        25ten büyükse obez */
+        {
+         
+            Console.WriteLine(  "lütfen kilonuzu giriniz");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(  "lütfen boyunuzu giriniz");
+            double boy = Convert.ToDouble (Console.ReadLine());
+            double sonuc = kilo / (boy * boy);
+           
+            if (sonuc <= 18)
+            {
+                Console.WriteLine("beden kitle indeksiniz =ZAYIF");
+                Console.ReadLine();
+            }
+            else if (sonuc>18 && sonuc < 25)
+            {
+                Console.WriteLine(  "beden kitle indeksiniz =NORMAL");
+                Console.ReadLine();
+            }
+            else if (sonuc>25)
+            {
+                Console.WriteLine("beden kitle indeksiniz =OBEZ");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
