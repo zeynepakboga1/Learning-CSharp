a8434bd297cde7590ce4746168e06bcdeae27759
Zeynep Akboğa <zeynepakboga2236@gmail.com>
2024-03-22 06:58:10 -0400

    carpim_tablosu

---
+++ Loops/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //birden ona kadar olan sayıları alt alta  ekrana yazdırma 
+             for (int i = 1; i <= 10; i++)
+             {
+                 Console.WriteLine(i);
+
+             }
+             Console.ReadLine();
+             //birden ona kadar olan çift sayıları ekrana yazdurma
+             for (int a = 0; a <= 10; a += 2)
+             {
+                 Console.WriteLine(a);
+             }
+             Console.ReadLine();
+             //iç içe for döngüsü örnek 1
+             for (int a = 1; a <= 5; a++)
+             {
+                 for (int b = 1; b <= a; b++) 
+                 {
+                     Console.Write(b);
+
+                 }
+
+                 Console.Write("");
+
+               Console.ReadLine();
+             }
+             //iç içe for örnek 2
+             for (int i = 1; i <= 5; i++)
+             {
+                 for (int j = 1; j <= i; j++)
+                 {
+                     Console.Write("*");
+                 }
+                 Console.WriteLine();
+                 Console.ReadLine();
+             }
+            
+            //while döngüsü ile 1 den 100 1e kadar yazdırma
+            int q = 1;
+            while (q <= 100) 
+            {
+                Console.WriteLine(  q);
+                q++;
+                    
+             
+            }
+            Console.ReadLine();
+            //100 den geriye çifterli yazdırma
+           int y = 100;
+            while (y >= 0)
+            {
+                Console.WriteLine(y);
+                y -= 2;
+            }
+            Console.ReadLine();
+
+
+        } 
+
+
+
+}   }
+
---
+++ carpim_tablosu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("carpim_tablosu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("carpim_tablosu")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("1fc61a46-bb4c-4a42-ade1-46f54f01d9ae")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
